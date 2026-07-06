812627b6587476bdbd13de182e443f139bc0dfe5
Victor Terra <victorterra221@gmail.com>
2026-07-06 16:11:16 -0300

    add comentario sobre dica de implementacao

--- modules/clook/clook.c
+++ modules/clook/clook.c
@@ -13,4 +13,16 @@ static struct elevator_type clook = {
     .elevator_owner = THIS_MODULE,
 };
 
+/*
+Outras funções úteis:
 
+    blk_mq_run_hw_queue(hctx, true): força o kernel a disparar uma chamada do has_work;
+    blk_rq_pos(rq): retorna o número do setor de uma requisição struct request *rq;
+
+Outras dicas:
+
+    Use linux/hrtimer.h para implementar o temporizador. Já trabalhamos com essa API no laboratório 2.4;
+    As callbacks insert_requests e dispatch_request podem ser chamadas concorrentemente, mas não podem ser postas para dormir por uma mutex. Para resolver, use uma spinlock quando for acessar a fila do C-LOOK;
+    O hrtimer pode modificar o estado dos dados do C-LOOK, necessitando também o uso da spinlock. Porém, o hrtimer usa dentro de uma interrupção. Portanto, sempre use spin_lock_irqsave e spin_lock_irqrestore ao invés de spin_lock e spin_unlock. Verifique a documentação da spinlock no Moodle.
+
+ */
