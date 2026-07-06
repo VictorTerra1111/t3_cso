5e3b4dec1acdcdd26c5673a0b38385332444a16b
Victor Terra <victorterra221@gmail.com>
2026-07-06 16:09:45 -0300

    add struct no clook

--- modules/clook/clook.c
+++ modules/clook/clook.c
@@ -1,1 +1,16 @@
 #include "elevator.h"
+
+static struct elevator_type clook = {
+    .ops = {
+        .init_sched = clook_init_sched,
+        .exit_sched = clook_exit_sched,
+        .insert_requests = clook_insert_requests,
+        .dispatch_request = clook_dispatch_request,
+        .has_work = clook_has_work,
+        .finish_request = clook_finish_request
+    },
+    .elevator_name = "clook",
+    .elevator_owner = THIS_MODULE,
+};
+
+
